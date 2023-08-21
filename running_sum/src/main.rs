f7b1842a9e25a0da4bf1f845eba5ecf25d512259
BigbysHand <rdbrhys1@gmail.com>
2023-08-21 16:00:01 -0300

    Rust code for running sum vector

---
+++ running_sum/src/main.rs
@@ -0,0 +1,11 @@
+pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+    let mut tmp: i32 = 0;
+    let mut roll: i32;
+    let mut out: Vec<i32> = Vec::new();
+    for num in nums{
+        roll = num + tmp;
+        tmp = roll;
+        out.push(roll);
+    }
+    return out;
+}
