7c8139d9a6b77023e8ec8d6297eef67d62f72b7b
BigbysHand <rdbrhys1@gmail.com>
2023-08-21 16:04:20 -0300

    returns value of richest account

---
+++ richest_customer/src/main.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
+        let mut max: i32 = 0;
+        for i in &accounts{
+
+            let mut wealth: i32 = 0;
+            for val in i{
+
+                wealth += val;
+            }
+            if wealth > max{
+                max = wealth;
+            }
+        }
+        return max;
+    }
+}
