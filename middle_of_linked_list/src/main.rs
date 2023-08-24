e233446dc345d6e3ae91200035b97a6974335651
BigbysHand <rdbrhys1@gmail.com>
2023-08-24 15:01:03 -0300

    returns middle node of singly linked list

--- middle_of_linked_list/src/main.rs
+++ middle_of_linked_list/src/main.rs
@@ -1,18 +1,34 @@
-impl Solution {
-    pub fn middle_node(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        let mut pointer = head.clone();
-        let mut length: i32 = 0;
-        while head != None{
-            length += 1;
-            head = head.unwrap().next;
-        }
-        for i in 0..(length /2){
-            pointer = pointer.unwrap().next;
-        }
-        return pointer;
-    }
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
 }
 
+impl ListNode {
+  //#[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+
+
+pub fn middle_node(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut pointer: Option<Box<ListNode>> = head.clone();
+    let mut length: i32 = 0;
+    while head != None{
+        length += 1;
+        head = head.unwrap().next;
+    }
+    for i in 0..(length /2){
+        pointer = pointer.unwrap().next;
+    }
+    return pointer;
+}
+
+
 fn main() {
-    println!("Hello, world!");
 }
